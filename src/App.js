de963f48c00085729a2d45a56b7e440e2df91f79
Anuj C More <moreanuj1307@gmail.com>
2022-02-27 10:47:57 +0530

    BG image added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Login />
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
